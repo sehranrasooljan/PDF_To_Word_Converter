310ae8f1db2e793cefcc0ed432a8cf5cc240a151
Sehran Rasool Jan <89655415+sehranrasooljan@users.noreply.github.com>
2024-01-06 20:07:30 +0530

    Pdftodocx.py

--- Pdftodocx.py
+++ Pdftodocx.py
@@ -1,6 +1,6 @@
 from pdf2docx import Converter
-pdf_file = 'consolidated_report.pdf'
-word_file  = 'consolidated_report.docx'
+pdf_file = 'demo.pdf'
+word_file  = 'demo.docx'
 #converter
 cv = Converter(pdf_file)
 cv.convert(word_file,start=0,end= None)
